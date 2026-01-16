45ee25dae5a36d8b6822b9531b19037df1363f3f
Ahamed Kabeer <ak.tech.27.coder@gmail.com>
2026-01-16 12:33:49 +0530

    Basic hand marking done

--- src/py_gesture_control/main.py
+++ src/py_gesture_control/main.py
@@ -10,15 +10,42 @@ VisionRunningMode = mp.tasks.vision.RunningMode
 
 
 MODEL_PATH = "src/models/hand_landmarker.task"
+HAND_CONNECTIONS = [
+    (0, 1),
+    (1, 2),
+    (2, 3),
+    (3, 4),
+    (0, 5),
+    (5, 6),
+    (6, 7),
+    (7, 8),
+    (0, 9),
+    (9, 10),
+    (10, 11),
+    (11, 12),
+    (0, 13),
+    (13, 14),
+    (14, 15),
+    (15, 16),
+    (0, 17),
+    (17, 18),
+    (18, 19),
+    (19, 20),
+]
+
+hand_land_res = None
 
 
 def print_result(
     result: HandLandmarkerResult, output_image: mp.Image, timestamp_ms: int
 ):
     if result.hand_landmarks:
-        logger.info(f"Hands detected: {len(result.hand_landmarks)}")
+        # logger.info(f"Hands detected: {len(result.hand_landmarks)}")
+        global hand_land_res
+        hand_land_res = result.hand_landmarks
     else:
-        logger.info("No hands")
+        hand_land_res = None
+        # logger.info("No hands")
 
 
 def main() -> None:
@@ -28,7 +55,7 @@ def main() -> None:
         base_options=BaseOptions(model_asset_path=MODEL_PATH),
         running_mode=VisionRunningMode.LIVE_STREAM,
         result_callback=print_result,
-        num_hands=2,
+        num_hands=6,
     )
     with HandLandmarker.create_from_options(hand_landmark_options) as landmarker:
         cap = cv.VideoCapture(0, cv.CAP_DSHOW)
@@ -55,6 +82,24 @@ def main() -> None:
 
             # Async detection
             landmarker.detect_async(mp_image, timestamp_ms)
+
+            if hand_land_res:
+                h, w, _ = frame.shape
+                for hand in hand_land_res:
+                    for start_idx, end_idx in HAND_CONNECTIONS:
+                        p1 = hand[start_idx]
+                        p2 = hand[end_idx]
+
+                        x1, y1 = int(p1.x * w), int(p1.y * h)
+                        x2, y2 = int(p2.x * w), int(p2.y * h)
+
+                        cv.line(frame, (x1, y1), (x2, y2), (255, 0, 0), 2)
+
+                    for lm in hand:
+                        x = int(lm.x * w)
+                        y = int(lm.y * h)
+
+                        cv.circle(frame, (x, y), 4, (0, 255, 0), -1)
 
             cv.imshow("Webcam Feed", frame)
 
